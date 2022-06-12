2d301f8e2afc9e6d6148c52d40a5d776d3f496cc
zuri227 <zuri22780@gmail.com>
2022-06-11 20:47:52 -0400

    add exports to userSlice component

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -24,4 +24,16 @@ const userSlice = createSlice({
   },
 })
 
-export default userSlice;
+export const {
+  setSignedIn,
+  setUserData,
+  setInput,
+  setBlogData,
+} = userSlice.actions
+
+export const selectSignedIn = (state) => state.user.isSignedIn
+export const selectUserData = (state) => state.user.userData
+export const selectUserInput = (state) => state.user.searchInput
+export const selectBlogData = (state) => state.user.blogData
+
+export default userSlice.reducer
