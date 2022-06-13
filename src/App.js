bdcda42f98e63ae9ceae726b44d714e60f1358a9
zuri227 <zuri22780@gmail.com>
2022-06-12 23:19:53 -0400

    Set up HomePage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to My Blog Website! </h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSignedIn, setSignedIn, setUserData } from "../features/userSlice";
+import "../styling/home.css"
+
+const HomePage = () => {
+  return ( 
+    <div>
+      Welcome to Homepage
+    </div>
+   );
+}
+ 
+export default HomePage;
