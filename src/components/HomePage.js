6db0349eb158a4a249b3998dcf8b7810e1f80b12
zuri227 <zuri22780@gmail.com>
2022-07-24 20:06:59 -0400

    Complete HomePage component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,9 +5,44 @@ import { selectSignedIn, setSignedIn, setUserData } from "../features/userSlice"
 import "../styling/home.css"
 
 const HomePage = () => {
+  const isSignedIn = useSelector(selectSignedIn)
+
+  const dispatch = useDispatch()
+  const login = (response) => {
+    console.log(response)
+    dispatch(setSignedIn(true))
+    dispatch(setUserData(response.profileObj))
+  }
+
   return ( 
-    <div>
-      Welcome to Homepage
+    <div className="home_page" style={{ display: isSignedIn ? "none" : ""}}> 
+      {!isSignedIn ? (
+        <div className="login_message">
+          <h2>📚</h2>
+          <h1>A Reader's Favorite Place!</h1>
+          <p>
+            We provide high quality online resources for reading. Just sign up and start reading.
+          </p>
+          <GoogleLogin
+          clientId="426449746429-k2il2jgi84m4ossmka4h8lsqbo9tbm1v.apps.googleusercontent.com"
+          render={(renderProps) => (
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+              className="login_button"
+            >
+              Login with Google
+            </button>
+          )}
+          onSuccess={login}
+          onFailure={login}
+          isSignedIn={true}
+          cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      ) : (
+        ""
+      )}
     </div>
     );
 }
